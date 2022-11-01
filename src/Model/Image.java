016c4499928eb16de758c50902f9c2c9385f3039
drewskotarczak <skotarczak.d@northeastern.edu>
2022-11-01 08:07:40 -0400

    drew added a getMetaData function and wrote simple javadoc comments for the class

--- src/Model/Image.java
+++ src/Model/Image.java
@@ -1,5 +1,8 @@
 package Model;
 
+/**
+ * A class representing a ppm image file with meta data separate from data.
+ */
 public class Image {
   private int height;
   private int width;
@@ -20,6 +23,10 @@ public class Image {
     this.pixels = pixels;
   }
 
+  /**
+   * translate this image object to a string of it's contents.
+   * @return a string in the format of a ppm file
+   */
   public String toString() {
     String pixelsString = "";
     for (Pixel[] rowOfPixels : pixels) {
@@ -30,6 +37,21 @@ public class Image {
     return "P3\n" + this.height + "\n" + this.width + "\n" + "255\n" + pixelsString;
   }
 
+  /**
+   * retrieve the width and height of the image object.
+   * @return returns the width and height in the form of an int list
+   */
+  public int[] getMetaData() {
+    int[] metaData = new int[2];
+    metaData[0] = this.width;
+    metaData[1] = this.height;
+    return metaData.clone();
+  }
+
+  /**
+   * retrieve a clone of the 2d list of pixels of this image
+   * @return returns a 2d list of Pixel objects
+   */
   public Pixel[][] getPixels() {
     return this.pixels.clone();
   }
