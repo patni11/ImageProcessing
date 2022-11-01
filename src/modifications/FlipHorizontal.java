baa295fd39f6be8ad32e355baeba743ffd0583ee
drewskotarczak <skotarczak.d@northeastern.edu>
2022-11-01 08:34:48 -0400

    finished both flip classes and added a clone() method to Pixel

--- src/modifications/FlipHorizontal.java
+++ src/modifications/FlipHorizontal.java
@@ -8,7 +8,23 @@ import Model.Pixel;
  */
 public class FlipHorizontal implements PPMModification{
 
+  /**
+   * flip the image horizontally.
+   * @param image the original image to be flipped
+   * @return a new image that is the same as the original but flipped horizontally
+   */
   public Image modifyImage(Image image) {
-    return new Image(4,5, new Pixel[][]{});
+    int width = image.getMetaData()[0];
+    int height = image.getMetaData()[1];
+    Pixel[][] originalPixels = image.getPixels();
+    //create a new 2d list of Pixels
+    Pixel[][] newPixels = new Pixel[height][width];
+    //go through all pixels in the original image
+    for (int i = 0; i < height; i ++) {
+      for (int j = 0; j < width; j ++) {
+        newPixels[i][width - j] = originalPixels[i][j].clone();
+      }
+    }
+    return new Image(height, width, newPixels);
   }
 }
--- src/modifications/FlipVertical.java
+++ src/modifications/FlipVertical.java
@@ -8,8 +8,23 @@ import Model.Pixel;
  */
 public class FlipVertical implements PPMModification {
 
-
+  /**
+   * flip the image vertically.
+   * @param image the original image to be flipped
+   * @return a new image that is the same as the original but flipped vertically
+   */
   public Image modifyImage(Image image) {
-    return new Image(4,5, new Pixel[][]{});
+    int width = image.getMetaData()[0];
+    int height = image.getMetaData()[1];
+    Pixel[][] originalPixels = image.getPixels();
+    //create a new 2d list of Pixels
+    Pixel[][] newPixels = new Pixel[height][width];
+    //go through all pixels in the original image
+    for (int i = 0; i < height; i++) {
+      for (int j = 0; j < width; j++) {
+        newPixels[height - i][j] = originalPixels[i][j].clone();
+      }
+    }
+    return new Image(height, width, newPixels);
   }
 }
