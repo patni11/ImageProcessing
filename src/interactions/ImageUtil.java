45aa05ee4ea19d732c806e53a3f16621b44fa5d6
shubh patni <shubhpatni2002@gmail.com>
2022-11-04 11:38:52 -0400

    added script, new mainTest file, made controller, and a res folder. And took changes from drew

--- src/interactions/ImageUtil.java
+++ src/interactions/ImageUtil.java
@@ -9,8 +9,7 @@ import model.Pixel;
 
 
 /**
- * This class contains utility methods to read a PPM image from file and simply print its contents.
- * Feel free to change this method as required.
+ * This class contains utility methods to read a PPM image from file.
  */
 public class ImageUtil {
 
