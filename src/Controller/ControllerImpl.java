13e46fea53de9da348f98448fef66137f72bc8bd
shubh patni <shubhpatni2002@gmail.com>
2022-11-04 11:32:04 -0400

    added script, new mainTest file, made controller, and a res folder.

---
+++ src/Controller/ControllerImpl.java
@@ -0,0 +1,108 @@
+package Controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.Function;
+
+import model.ImageStorage;
+import modifications.Brighten;
+import modifications.Flip;
+import modifications.Greyscale;
+import modifications.Load;
+import modifications.PPMModification;
+import modifications.Save;
+
+/**
+ * Controller impleneation for the image processing controller.
+ * This will run the software.
+ */
+public class ControllerImpl implements ImageProcessingController {
+
+  Scanner sc;
+  ImageStorage imgStorage;
+  Map<String, Function<Scanner, PPMModification>> knownCommands;
+
+  /**
+   * Constructor for the image processing program controller.
+   * @param input takes in user input
+   * @param imgStorage takes a storage
+   */
+  public ControllerImpl(Readable input, ImageStorage imgStorage ) {
+    if (input == null || imgStorage == null) {
+      throw new IllegalArgumentException("Either storage or input is null");
+    }
+
+    this.sc = new Scanner(input);
+    this.imgStorage = imgStorage;
+    this.knownCommands = new HashMap<>();
+    knownCommands.put("load", (Scanner s) -> {
+      return new Load(imgStorage, s.next(), s.next());
+    });
+
+    knownCommands.put("brighten", (Scanner s) -> {
+      return new Brighten(imgStorage, s.nextInt(), s.next(), s.next());
+    });
+
+    knownCommands.put("vertical-flip", (Scanner s) -> {
+      return new Flip(imgStorage, "vertical", sc.next(), sc.next());
+    });
+
+    knownCommands.put("horizontal-flip", (Scanner s) -> {
+      return new Flip(imgStorage, "horizontal", sc.next(), sc.next());
+    });
+
+    knownCommands.put("value-component", (Scanner s) -> {
+      return new Greyscale(imgStorage, "value-component", sc.next(), sc.next());
+    });
+
+    knownCommands.put("red-component", (Scanner s) -> {
+      return new Greyscale(imgStorage, "red-component", sc.next(), sc.next());
+    });
+
+    knownCommands.put("green-component", (Scanner s) -> {
+      return new Greyscale(imgStorage, "green-component", sc.next(), sc.next());
+    });
+
+    knownCommands.put("blue-component", (Scanner s) -> {
+      return new Greyscale(imgStorage, "blue-component", sc.next(), sc.next());
+    });
+
+    knownCommands.put("luma-component", (Scanner s) -> {
+      return new Greyscale(imgStorage, "luma-component", sc.next(), sc.next());
+    });
+
+    knownCommands.put("intensity-component", (Scanner s) -> {
+      return new Greyscale(imgStorage, "intensity-component", sc.next(), sc.next());
+    });
+
+    knownCommands.put("save", (Scanner s) -> {
+      return new Save(imgStorage, sc.next(), sc.next());
+    });
+  }
+
+  @Override
+  public void startEditor() {
+    while (sc.hasNext()) {
+      String in = sc.next();
+      if (in.equalsIgnoreCase("q") || in.equalsIgnoreCase("quit")) {
+        return;
+      }
+
+      Function<Scanner, PPMModification> cmd = knownCommands.getOrDefault(in, null);
+
+      if (cmd == null) {
+        throw new IllegalArgumentException("Invalid Command");
+
+      } else {
+        PPMModification c = cmd.apply(sc);
+        try {
+          c.runFunction();
+        } catch (Exception e) {
+          System.out.println(e);
+        }
+      }
+    }
+  }
+
+}
